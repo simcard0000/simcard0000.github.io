1bd4d60895450b68759ba61e6c77fe013d8051d8
simcard0000 <simran.thind@outlook.com>
2024-02-08 10:23:29 -0500

    bring back the extended option lol

--- netlify/functions/last_fm.mjs
+++ netlify/functions/last_fm.mjs
@@ -1,5 +1,5 @@
 export default async (request, context) => {
     const api_key = Netlify.env.get("LAST_FM_KEY");
-    const response = await fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=SuperNova_-_&limit=1&api_key=" + api_key + "&format=json")
+    const response = await fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=SuperNova_-_&extended=1&limit=1&api_key=" + api_key + "&format=json")
     return response;
 }
