8b36ca2dd7ccab063a251f1a666d89aafe1d00b1
simcard0000 <simran.thind@outlook.com>
2024-02-08 10:10:50 -0500

    remove extended option

--- netlify/functions/last_fm.mjs
+++ netlify/functions/last_fm.mjs
@@ -1,6 +1,5 @@
 export default async (request, context) => {
     const api_key = Netlify.env.get("LAST_FM_KEY");
-    const response = await fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=SuperNova_-_&limit=1&extended=1&api_key=" + api_key + "&format=json")
-    let ret_response = response.clone();
-    return ret_response;
+    const response = await fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=SuperNova_-_&limit=1&api_key=" + api_key + "&format=json")
+    return response;
 }
