d31d6c954c281d95c304fa08dd29ea0fa5f13a47
simcard0000 <simran.thind@outlook.com>
2024-02-10 13:44:14 -0500

    please Steam work

--- netlify/functions/steam.mjs
+++ netlify/functions/steam.mjs
@@ -1,6 +1,6 @@
 export default async (request, context) => {
     const api_key = Netlify.env.get("STEAM_KEY");
     const username = Netlify.env.get("STEAM_NAME")
-    const response = await fetch("https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=" + api_key + "&steamid=" + username + "&count=1&format=json")
+    const response = await fetch("https://partner.steam-api.com/IPlayerService/GetRecentlyPlayedGames/v1/?key=" + api_key + "&input_json={\"steamid\":" + username + ", \"count\":1}")
     return response;
 }
