b6fb0c1167d4b098807afe89ba7065d683271a01
simcard0000 <simran.thind@outlook.com>
2024-02-10 14:01:55 -0500

    changing URL again

--- netlify/functions/steam.mjs
+++ netlify/functions/steam.mjs
@@ -1,6 +1,6 @@
 export default async (request, context) => {
     const api_key = Netlify.env.get("STEAM_KEY");
     const username = Netlify.env.get("STEAM_NAME")
-    const response = await fetch("https://partner.steam-api.com/IPlayerService/GetRecentlyPlayedGames/v1/?key=" + api_key + "&input_json={\"steamid\":" + username + ", \"count\":1}")
+    const response = await fetch("https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/?key=" + api_key + "&input_json={\"steamid\":" + username + "}")
     return response;
 }
