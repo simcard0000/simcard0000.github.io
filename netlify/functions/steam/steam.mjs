fd8010be3b110e6a581bdd31a57ce5a0ad37cfc0
simcard0000 <simran.thind@outlook.com>
2024-02-10 18:25:33 -0500

    ok let us make a new Response object

--- netlify/functions/steam/steam.mjs
+++ netlify/functions/steam/steam.mjs
@@ -3,7 +3,5 @@ export default async (request, context) => {
     const username = Netlify.env.get("STEAM_NAME")
     const response = await fetch("https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?format=json&key=" + api_key + "&steamid=" + username)
     const response_json = await response.json();
-    console.log(response_json);
-    console.log(response.headers);
-    return response;
+    return new Response(response_json);
 }
