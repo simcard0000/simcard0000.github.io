2a7c143606e174d20a12cf5fad001d2a0da8dde3
simcard0000 <simran.thind@outlook.com>
2024-02-11 15:15:39 -0500

    testing out wildcard

--- netlify/functions/steam/steam.mjs
+++ netlify/functions/steam/steam.mjs
@@ -3,5 +3,12 @@ export default async (request, context) => {
     const username = Netlify.env.get("STEAM_NAME")
     const response = await fetch("https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?format=json&key=" + api_key + "&steamid=" + username)
     const response_json = await response.json();
-    return new Response(response_json);
+    let headers = new Headers();
+    headers.append("Access-Control-Allow-Origin", "*");
+    const options = {
+        status: 200,
+        statusText: "OK",
+        headers
+    }
+    return new Response(response_json, options);
 }
